55dc701868350bc9d1ca7d29533efa8a3e1ee7fc
Diogo Corte <diogo.corte@ua.pt>
2016-09-28 12:11:55 +0100

    added ex3 .. cp from ex2

---
+++ src/classes/exercises03/a3_skinColor/main.cpp
@@ -0,0 +1,47 @@
+#include <cv.h>
+#include <highgui.h>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char **argv) {
+    VideoCapture cap(0); // open the default camera
+    if(!cap.isOpened())  // check if we succeeded
+        return -1;
+
+    Mat frame;
+
+    int lastState;
+    while (true) {
+        cap >> frame;
+
+        char received = (char)waitKey(10);
+        if (received == '+')
+            lastState = (lastState + 1) % 5;
+        else if (received == '-')
+            lastState = lastState==0 ? 4 : lastState-1;
+        else if (received == 'q' || received == 'Q')
+            break;
+
+        switch (lastState) {
+            case 1:
+                cvtColor(frame, frame, CV_BGR2GRAY);
+                break;
+            case 2:
+                cvtColor(frame, frame, CV_BGR2GRAY);
+                threshold(frame,frame, 127, 255, THRESH_BINARY);
+                break;
+            case 3:
+                cvtColor(frame, frame, CV_BGR2GRAY);
+                adaptiveThreshold(frame, frame, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 3, 0);
+                break;
+            case 4:
+                cvtColor(frame, frame, CV_BGR2GRAY);
+                adaptiveThreshold(frame, frame, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 3, 0);
+        }
+        namedWindow("Camera", CV_WINDOW_AUTOSIZE);
+        imshow("Camera", frame);
+    }
+    return 0;
+}
