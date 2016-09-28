4c2361344c9e9eaab2de7fb85ce1484d07081152
Diogo Silva <dbtds@ua.pt>
2016-09-28 10:57:05 +0100

    Added new class

---
+++ src/classes/exercises03/a1_adaptiveThreshold/main.cpp
@@ -0,0 +1,16 @@
+#include <cv.h>
+#include <highgui.h>
+#include <opencv2/opencv.hpp>
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <stdio.h>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char **argv) {
+    
+    waitKey(0);
+
+    return 0;
+}
