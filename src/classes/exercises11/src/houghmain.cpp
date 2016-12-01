c08a91a63ec663013dad85f83ad789bd62691a10
Diogo Silva <dbtds@ua.pt>
2016-12-01 21:57:20 +0000

    Hough last version

--- src/classes/exercises11/src/houghmain.cpp
+++ src/classes/exercises11/src/houghmain.cpp
@@ -22,50 +22,40 @@ int main(int argc, const char * argv[]) {
     moveWindow("", 0, 0);
     
     Mat image = imread(argv[1]);
-    resize( image, image, Size(), 2.0, 2.0 );
+    resize( image, image, Size(), 1.0, 1.0 );
     
-    const double resize_factor_x = 2;
+    const double resize_factor_x = 1;
     const double resize_factor_y = 1;
     
     /* Find the edges of the image */
     Mat gray, edges;
     cvtColor( image, gray, CV_BGR2GRAY );
-    Canny( gray, edges, 100, 300, 3 );
-    
-    
-    /* Initialize the Hough accumulation matrix */
-    Hough hough;
-    hough.init( edges );    
-    
-    /* Make the Canny edges, blue, real blue */
-    vector<Mat> temp = {
-        edges,
-        Mat::zeros( edges.size(), CV_8UC1),
-        Mat::zeros( edges.size(), CV_8UC1)
-    };
-    merge( temp, edges );
-    
-    
-    /* Get the size of accum matrix in advance, for the mouse call back purpose  */
-    Mat accum = hough.getAccumulationMatrix();
-    resize( accum, accum, Size(), 2.0 * resize_factor_x, 0.5 * resize_factor_y );
-    Rect region( image.cols, 0, accum.cols, accum.rows );
-    setMouseCallback( "", onMouse, static_cast<void*>( &region ) );
-    
-    
-    /* Final output matrix, will be combination of the original image and the accumulation matrix */
-    Mat appended( MAX( accum.rows, image.rows ), image.cols + accum.cols, CV_8UC3, Scalar(0, 0, 0) );
     
     
     bool show_canny = false;
     int threshold = 0;
-    createTrackbar( "Hough threshold", "", &threshold, 1000 );
-    
-    char str[255];    
+    int ratio = 3;
+    int canny_thresh = 100;
     while( true ) {
+        Canny( gray, edges, canny_thresh, canny_thresh * ratio, 3 );
+        Hough hough;
+        hough.init( edges );   
+        /* Get the size of accum matrix in advance, for the mouse call back purpose  */
+        Mat accum = hough.getAccumulationMatrix();
+        resize( accum, accum, Size(), 2.0 * resize_factor_x, 0.5 * resize_factor_y );
+
+        Rect region( image.cols, 0, accum.cols, accum.rows );
+        setMouseCallback( "", onMouse, static_cast<void*>( &region ) );
+        /* Final output matrix, will be combination of the original image and the accumulation matrix */
+        Mat appended( MAX( accum.rows, image.rows ), image.cols + accum.cols, CV_8UC3, Scalar(0, 0, 0) );
+
+        char str[255];   
+        createTrackbar( "Hough threshold", "", &threshold, 1000 ); 
+        createTrackbar( "First threshold", "", &canny_thresh, 400 ); 
+        createTrackbar( "Canny ratio (Second threshold = first * ratio)", "", &ratio, 10 ); 
         Mat temp = image.clone();
-        if( show_canny )
-            temp += edges;
+        //if( show_canny )
+        //    temp += edges;
         
         /* Try to visualize the accumulation matrix */
         accum = hough.getAccumulationMatrix( threshold );
@@ -73,7 +63,7 @@ int main(int argc, const char * argv[]) {
         equalizeHist( accum, accum );
         
         /* Apply colormap for better representation of the accum matrix */
-        applyColorMap( accum, accum, cv::COLORMAP_BONE );
+        applyColorMap( accum, accum, cv::COLORMAP_OCEAN );
         resize( accum, accum, Size(), 2.0 * resize_factor_x, 0.5 * resize_factor_y);
         
         /* Draw the lines based on threshold */
@@ -88,8 +78,6 @@ int main(int argc, const char * argv[]) {
             line( temp, point_pair.first, point_pair.second, CV_RGB(0, 255, 0), 1 );
         }
         
-
-        
         /* Copy everything to output matrix */
         appended = Scalar::all(0);
         temp.copyTo ( Mat(appended, Rect(0, 0, temp.cols, temp.rows)) );
@@ -97,8 +85,6 @@ int main(int argc, const char * argv[]) {
         
         
         /* Output some text */
-        putText( appended, "Accum matrix", cvPoint(10, 40), FONT_HERSHEY_COMPLEX_SMALL, 1, cvScalar(255,255,255), 1, CV_AA);
-        putText( appended, "[C] to show Canny edges", cvPoint( 10, 60 ), FONT_HERSHEY_COMPLEX_SMALL, 1, cvScalar(255,255,255), 1, CV_AA);
         putText( appended, "[Q] to quit", cvPoint( 10, 80 ), FONT_HERSHEY_COMPLEX_SMALL, 1, cvScalar(255,255,255), 1, CV_AA);
         sprintf( str, "Threshold: %d", threshold );
         putText( appended, str, cvPoint( 10, 100 ), FONT_HERSHEY_COMPLEX_SMALL, 1, cvScalar(255,255,255), 1, CV_AA);
@@ -107,11 +93,10 @@ int main(int argc, const char * argv[]) {
 
         
         imshow( "", appended );
+        imshow( "Canny", edges );
         char key = waitKey(10);
         if( key == 'q' )
             break;
-        else if( key == 'c' )
-            show_canny = !show_canny;
     }
     
     return 0;
@@ -122,7 +107,6 @@ int main(int argc, const char * argv[]) {
  * Mouse callback, to show the line based on which part of accumulation matrix the cursor is.
  */
 static void onMouse( int event, int x, int y, int, void * data ) {
-    cout << "Clicked " << x << ", " << y << endl;
     Rect *region = (Rect*) data;
     Point point( x, y );
     
