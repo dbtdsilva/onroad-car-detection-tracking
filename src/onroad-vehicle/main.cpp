7ff161f8d77ed864c161b79fbd794bb5211f31a0
Diogo Silva <dbtds@ua.pt>
2016-12-21 17:12:26 +0000

    Haar cascades is now being represented with squares

--- src/onroad-vehicle/main.cpp
+++ src/onroad-vehicle/main.cpp
@@ -29,13 +29,25 @@ int main(int argc, const char** argv)
     }
 
     int keyboard;
+
+    vector<Rect> cars;
+    Mat frame_gray;
     do {
         if (!capture.read(frame)) {
             cerr << "Unable to read next frame." << endl;
             cerr << "Exiting..." << endl;
             exit(EXIT_FAILURE);
         }
+
+        cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+        equalizeHist(frame_gray, frame_gray);
+
+        car_cascade.detectMultiScale(frame_gray, cars, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30));
+        for (const auto& car : cars) {
+            rectangle(frame, car, Scalar(0, 255, 0), 2);
+        }
         imshow("Camera", frame);
+
         keyboard = waitKey( 30 );
     } while((char)keyboard != 'q' && (char)keyboard != 'Q' && keyboard != 27 );
 
